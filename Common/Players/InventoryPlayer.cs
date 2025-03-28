04924ebd559086b2a38241ed8d0c4543a6ead24d
jacky <jackykim573@gmail.com>
2025-03-28 18:44:32 +1300

    Fixed tracking, only pickup tracks

--- Common/Players/InventoryPlayer.cs
+++ Common/Players/InventoryPlayer.cs
@@ -6,43 +6,43 @@ using System.Linq;
 
 namespace CalamityWeaponChecklist.Common.Players
 {
-	public class InventoryPlayer : ModPlayer
-	{
-		public IList<string> checklist;
+    public class InventoryPlayer : ModPlayer
+    {
+        public IList<string> checklist = new List<string>();
 
-		public override bool OnPickup(Item item)
-		{
-			Main.NewText(Player.name + " picked up " + item.Name, 0, 255, 255);
 
-			if (!checklist.Contains(item.Name))
-			{
-				checklist.Add(item.Name);
-			} 
+        public override bool OnPickup(Item item)
+        {
+            
+            if (checklist.Contains(item.Name))
+            {
+                Main.NewText(Player.name + " already has " + item.Name);
+                Main.NewText(Player.name + " list size is STILL " + checklist.Count);
+                return true;
+            }
+            else
+            {
+                checklist.Add(item.Name);
+                Main.NewText(Player.name + " picked up " + item.Name);
+                Main.NewText(Player.name + " INCREASED list size to " + checklist.Count);
+            }
+            return true;
+        }
 
-			return true;
-		}
+        public override void SaveData(TagCompound tag)
+        {
+            tag["CalamityWeaponChecklist"] = checklist;
+        }
 
-		public override void SaveData(TagCompound tag)
-		{
-			tag["CalamityWeaponChecklist"] = checklist;
-		}
+        public override void LoadData(TagCompound tag)
+        { 
+            checklist = tag.GetList<string>("CalamityWeaponChecklist");
+        }
 
-		public override void LoadData(TagCompound tag)
-		{
-			if (!tag.ContainsKey("CalamityWeaponChecklist"))
-			{
-				tag.Add("CalamityWeaponChecklist", new List<string>());
-			}
-			else
-			{
-				checklist = tag.GetList<string>("CalamityWeaponChecklist");
-			}
-		}
+        public override void OnEnterWorld()
+        {
+            Main.NewText(Player.name + " initial list size is " + checklist.Count) ;
+        }
+    }
 
-		public override void OnEnterWorld()
-		{
-
-		}
-
-	}
 }
